707189cdd606fdc89b454f941f3691a7d26f0c0b
razchiriac <raz.chiriac@gmail.com>
2017-07-25 10:54:58 -0400

    Finished App

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,17 +1,38 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { FlatList, Text } from 'react-native';
+import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
+import { Card } from './common';
 
 class EmployeeList extends Component {
+  componentWillMount() {
+    this.props.employeesFetch();
+  }
+  
+  renderItem({ item }) {
+    return <ListItem employee={item} />;
+  }
+  
   render() {
     return (
-      <View>
-        <Text>Employee list</Text>
-        <Text>Employee list</Text>
-        <Text>Employee list</Text>
-        <Text>Employee list</Text>
-      </View>
+      <Card style={{flex:1}}>
+        <FlatList
+          data={this.props.employees}
+          renderItem={this.renderItem}
+        />
+      </Card>
     );
   };
 };
 
-export default EmployeeList;
+const mapStateToProps = state => {
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid }; 
+  });
+  
+  return { employees };
+};
+
+export default connect(mapStateToProps, {employeesFetch})(EmployeeList);
--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,5 +1,7 @@
 import {
-  EMPLOYEE_UPDATE
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEE_SAVE_SUCCESS
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -9,11 +11,14 @@ const INITIAL_STATE = {
 };
 
 export default (state = INITIAL_STATE, action) => {
-  console.log(action);
   switch (action.type) {
     case EMPLOYEE_UPDATE:
       // action.payload === { prop: 'name', value: 'jane' }
       return { ...state, [action.payload.prop]: action.payload.value };
+    case EMPLOYEE_CREATE:
+      return INITIAL_STATE;
+    case EMPLOYEE_SAVE_SUCCESS:
+      return INITIAL_STATE;
     default:
       return state;
   };
